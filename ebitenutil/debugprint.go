81ff6a95a59d390c94f9b37708faf6ed43f5b64c
Hajime Hoshi <hajimehoshi@gmail.com>
2018-04-04 01:26:11 +0900

    graphics: Remove unused returning value from (*Image).DrawImage

--- ebitenutil/debugprint.go
+++ ebitenutil/debugprint.go
@@ -68,7 +68,7 @@ func drawDebugText(rt *ebiten.Image, str string, ox, oy int, shadow bool) {
 		op.GeoM.Reset()
 		op.GeoM.Translate(float64(x), float64(y))
 		op.GeoM.Translate(float64(ox+1), float64(oy))
-		_ = rt.DrawImage(debugPrintTextImage, op)
+		rt.DrawImage(debugPrintTextImage, op)
 		x += cw
 	}
 }
