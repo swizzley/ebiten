61ce87324e452509ca8109d9b2cec2021e275c7c
Hajime Hoshi <hajimehoshi@gmail.com>
2018-04-06 03:21:45 +0900

    graphics: Remove unused returning value from NewImageFromImage

--- examples/windowsize/main.go
+++ examples/windowsize/main.go
@@ -215,7 +215,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	gophersImage, _ = ebiten.NewImageFromImage(img, ebiten.FilterDefault)
+	gophersImage = ebiten.NewImageFromImage(img)
 
 	ebiten.SetWindowIcon([]image.Image{createRandomIconImage()})
 
